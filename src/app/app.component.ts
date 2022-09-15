2d26cd2a95f2558f31551d781e03dde323ff7eaf
Joshua Mitima <joshuamitima@dhcp-9-236-167-58.068-nl.ibmmobiledemo.com>
2022-09-15 13:06:04 +0200

    first commits

---
+++ src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  
+  onChangeLength( value: string ) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue; 
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+
+  onButtonClick() {
+    // console.log(`Letters: ${this.includeLetters}, Numbers: ${this.includeNumbers}, Symbols: ${this.includeSymbols}`)
+    // this.password = 'MY PASSWORD!!!';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklnmopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+}
